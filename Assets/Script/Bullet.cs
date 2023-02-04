39cd56dae7f9423ca7328cf4ffc1cb7958c77660
hector67100 <46929925+hector67100@users.noreply.github.com>
2023-02-04 11:23:05 -0400

    el enemigo se mueve y dispara hacia el jugador

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -8,9 +8,13 @@ public class Bullet : MonoBehaviour
     public float speed;
     // Start is called before the first frame update
     // Update is called once per frame
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
     void Update()
     {
-        transform.position += new Vector3 (direccion.x,direccion.y,0) * Time.deltaTime * speed;
+        transform.position += transform.up * Time.deltaTime * speed;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : MonoBehaviour
     public float x;
     public float y;
     public bool cambiarDireccion;
+    public GameObject jugador;
+    Quaternion rot;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +18,25 @@ public class Enemy : MonoBehaviour
         y = Random.Range(-1,1);
         mover = GetComponent<Move>();
         gun = GetComponent<Gun>();
+        jugador = GameObject.Find("Hero");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movimiento
         if(cambiarDireccion)
         { 
             StartCoroutine(volverAMover());
             cambiarDireccion = false;
         }
+        rotateToPlayer();
         mover.mover(new Vector3(x,y,0));
+
+        //Disparo
+        // float log = Mathf.Sqrt(2) - (int) Mathf.Sqrt(2);
+        // Debug.Log(log);
+        gun.Shot(transform.rotation);
     }
 
     public IEnumerator volverAMover()
@@ -35,5 +45,12 @@ public class Enemy : MonoBehaviour
         x = Random.Range(-1,1);
         y = Random.Range(-1,1);
         cambiarDireccion = true;
+    }
+
+    public void rotateToPlayer()
+    {
+        Vector3 dir = jugador.transform.position - transform.position;
+        rot = Quaternion.LookRotation(Vector3.forward, dir);
+        transform.rotation =  Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * 2f);
     }
 }
