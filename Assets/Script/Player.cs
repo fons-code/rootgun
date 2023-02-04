026c2c8ba7b6fe0a403e7f522336850e1364da84
Alessandrim <fons.code@gmail.com>
2023-02-04 11:21:32 -0400

    Add player rotation

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed=5f;
+    public Camera cam;
     public Rigidbody2D rb;
     Vector2 movement;
+    Vector2 mousePos;
     Move mover;
     Gun gun;
     // Start is called before the first frame update
@@ -21,8 +23,15 @@ public class Player : MonoBehaviour
     {
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
+
+       mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
     void FixedUpdate(){
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+
+        rb.rotation = angle;
     }
 }
