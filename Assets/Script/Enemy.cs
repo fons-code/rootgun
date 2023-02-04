b0ba57d85fb70ced751bf489680796263530f24f
hector67100 <46929925+hector67100@users.noreply.github.com>
2023-02-04 02:24:30 -0400

    ajustes al script de movimiento y el enemigo se mueve aleatoriamente

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Move mover;
+    Gun gun;
+    public float x;
+    public float y;
+    public bool cambiarDireccion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = Random.Range(-1,1);
+        y = Random.Range(-1,1);
+        mover = GetComponent<Move>();
+        gun = GetComponent<Gun>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(cambiarDireccion)
+        { 
+            StartCoroutine(volverAMover());
+            cambiarDireccion = false;
+        }
+        mover.mover(new Vector3(x,y,0));
+    }
+
+    public IEnumerator volverAMover()
+    {
+        yield return new WaitForSeconds(1f);
+        x = Random.Range(-1,1);
+        y = Random.Range(-1,1);
+        cambiarDireccion = true;
     }
 }
