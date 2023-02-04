915a5b2eeaa081eba43a6cbd07a2a70329204dc5
Alessandrim <fons.code@gmail.com>
2023-02-04 01:48:00 -0400

    Add Player movement

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed=5f;
+    float h;
+    float v;
+    Vector3 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       h = Input.GetAxis("Horizontal");
+       v = Input.GetAxis("Vertical");
+
+       moveDirection.x = h;
+       moveDirection.y = v;
+
+       transform.position += moveDirection * Time.deltaTime * speed; 
     }
 }
